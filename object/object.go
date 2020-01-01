9450f8bfac911421077762be316cc50a6af3c088
shoma07 <23730734+shoma07@users.noreply.github.com>
2020-01-02 02:16:12 +0900

    #04 組込関数を追加

--- object/object.go
+++ object/object.go
@@ -17,6 +17,7 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Object interface {
@@ -120,4 +121,17 @@ func (o *Function) Inspect() string {
 	out.WriteString("\n}")
 
 	return out.String()
+}
+
+// 組込関数
+type BuiltinFunction func(args ...Object) Object
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (o *Builtin) Type() ObjectType {
+	return BUILTIN_OBJ
+}
+func (o *Builtin) Inspect() string {
+	return "builtin function"
 }
